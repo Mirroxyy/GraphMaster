bc72b72ed2e2b3c4f7a1665c329f8af2e840fde3
grechka <166709638+Mirroxyy@users.noreply.github.com>
2026-05-15 21:54:34 +0700

    Add io.h header with I/O function declarations

--- src/io.h
+++ src/io.h
@@ -1,1 +1,34 @@
+#ifndef IO_H
+#define IO_H
 
+// Инициализация консольного интерфейса
+void io_init();
+
+// Главное меню: возвращает выбранный пункт (1-5) или 0 для выхода
+int io_main_menu();
+
+// Запрос имени файла
+char* io_ask_filename();
+
+// Запрос номера вершины
+int io_ask_vertex(int max_vertex);
+
+// Вывод сообщения
+void io_print(const char *msg);
+
+// Вывод результата Дейкстры
+void io_print_dijkstra(const int *path, int path_len, int total_weight);
+
+// Вывод результата MST
+void io_print_mst(const MSTResult *res);
+
+// Вывод результата BFS
+void io_print_bfs(const int *order, int order_len);
+
+// Вывод ошибки
+void io_error(const char *msg);
+
+// Очистка ресурсов
+void io_cleanup();
+
+#endif
